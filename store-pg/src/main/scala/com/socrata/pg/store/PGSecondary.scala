3f76187dc67e911baa4328d62adc60b83e7932a0
John V Kew II <john.kew@socrata.com>
2014-01-27 15:54:16 -0800

    Stubbed secondary store for postgres

---
+++ store-pg/src/main/scala/com/socrata/pg/store/PGSecondary.scala
@@ -0,0 +1,31 @@
+package com.socrata.pg.store
+
+import com.socrata.datacoordinator.secondary._
+import com.socrata.soql.types.{SoQLValue, SoQLType}
+import com.socrata.datacoordinator.util.collection.ColumnIdMap
+import com.socrata.datacoordinator.secondary.ColumnInfo
+import com.socrata.datacoordinator.secondary.CopyInfo
+import com.socrata.datacoordinator.secondary.DatasetInfo
+
+/**
+ * Postgres Secondary Store Implementation
+ */
+class PGSecondary extends Secondary[SoQLType, SoQLValue] {
+  def shutdown() = ???
+
+  def wantsWorkingCopies = ???
+
+  def dropDataset(datasetInternalName: String, cookie: Secondary.Cookie) = ???
+
+  def currentVersion(datasetInternalName: String, cookie: Secondary.Cookie) = ???
+
+  def currentCopyNumber(datasetInternalName: String, cookie: Secondary.Cookie) = ???
+
+  def snapshots(datasetInternalName: String, cookie: Secondary.Cookie) = ???
+
+  def dropCopy(datasetInternalName: String, copyNumber: Long, cookie: Secondary.Cookie) = ???
+
+  def version(datasetInfo: DatasetInfo, dataVersion: Long, cookie: Secondary.Cookie, events: Iterator[Event[SoQLType, SoQLValue]]) = ???
+
+  def resync(datasetInfo: DatasetInfo, copyInfo: CopyInfo, schema: ColumnIdMap[ColumnInfo[SoQLType]], cookie: Secondary.Cookie, rows: _root_.com.rojoma.simplearm.Managed[Iterator[ColumnIdMap[SoQLValue]]]) = ???
+}
