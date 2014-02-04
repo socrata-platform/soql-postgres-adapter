cb7826e011e621786dffe4c3ca6a263e772edf46
John Kew <john.kew@socrata.com>
2014-02-04 15:42:34 -0800

    Starting test setup for PGSecondary

---
+++ store-pg/src/test/scala/com/socrata/pg/store/PGSecondaryTest.scala
@@ -0,0 +1,25 @@
+package com.socrata.pg.store
+
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import org.scalatest.matchers.MustMatchers
+
+/**
+ * Test... the PGSecondary.
+ */
+class PGSecondaryTest  extends FunSuite with MustMatchers with BeforeAndAfterAll {
+
+
+
+  test("can handle working copy event") {
+
+  }
+
+  test("refuse to create working copies with copy ids != 1") {
+
+  }
+
+  test("fail when we create a new dataset with a copy id != 1") {
+
+  }
+
+}
