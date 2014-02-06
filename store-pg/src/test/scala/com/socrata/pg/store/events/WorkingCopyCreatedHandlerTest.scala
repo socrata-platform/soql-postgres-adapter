9b4afb63247f42ce85e74a659f05bb249731811c
John Kew <john.kew@socrata.com>
2014-02-06 07:01:03 -0800

    Another basic test for WorkingCopyCreatedHandler
    Conflicts:
    
    	store-pg/src/test/scala/com/socrata/pg/store/events/WorkingCopyCreatedHandlerTest.scala

--- store-pg/src/test/scala/com/socrata/pg/store/events/WorkingCopyCreatedHandlerTest.scala
+++ store-pg/src/test/scala/com/socrata/pg/store/events/WorkingCopyCreatedHandlerTest.scala
@@ -1,26 +1,34 @@
 package com.socrata.pg.store.events
 
-import com.socrata.datacoordinator.secondary.{LifecycleStage, CopyInfo, DatasetInfo}
-import com.socrata.datacoordinator.id.CopyId
-import com.socrata.pg.store.PGSecondaryTestBase
+import com.socrata.datacoordinator.secondary.{LifecycleStage, CopyInfo, DatasetInfo => SecondaryDatasetInfo}
+import com.socrata.datacoordinator.id.{DatasetId, CopyId}
+import com.socrata.pg.store.{PostgresUniverseCommon, PGSecondaryUniverse, PGSecondaryTestBase}
+import com.socrata.soql.types.{SoQLValue, SoQLType}
+import com.socrata.datacoordinator.truth.metadata.{DatasetInfo => TruthDatasetInfo}
 
 class WorkingCopyCreatedHandlerTest extends PGSecondaryTestBase {
   import com.socrata.pg.store.PGSecondaryUtil._
 
+  val datasetInfo = SecondaryDatasetInfo(testInternalName, localeName, obfuscationKey)
+  val dataVersion = 0L
+  val copyInfo = CopyInfo(new CopyId(-1), 1, LifecycleStage.Published, dataVersion)
+
   test("can handle working copy event") {
     withPgu() { pgu =>
-      val datasetInfo = DatasetInfo(testInternalName, localeName, obfuscationKey)
+      val datasetInfo = SecondaryDatasetInfo(testInternalName, localeName, obfuscationKey)
       val dataVersion = 0L
       val copyInfo = CopyInfo(new CopyId(-1), 1, LifecycleStage.Published, dataVersion)
       WorkingCopyCreatedHandler(pgu, datasetInfo, copyInfo)
     }
   }
 
-  test("refuse to create working copies with copy ids != 1") {
-
+  test("refuse to create copies with copy ids != 1; we do not support working copies") {
+    val copyInfo = CopyInfo(new CopyId(-1), -1, LifecycleStage.Published, dataVersion)
+    withPgu() { pgu =>
+      intercept[UnsupportedOperationException] {
+        WorkingCopyCreatedHandler(pgu,datasetInfo, copyInfo)
+      }
+    }
   }
 
-  test("fail when we create a new dataset with a copy id != 1") {
-
-  }
 }
