df7d31b0823de81e97f0e44a310b79a8931ae1c7
Chi Tang <chitang@mac2.local>
2014-03-20 09:09:14 -0700

    Expand text column indexing.  Add index for boolean and timestamp.
    Reviewed-by: john.kew@socrata.com

--- common-pg/src/main/scala/com/socrata/pg/store/index/Indexable.scala
+++ common-pg/src/main/scala/com/socrata/pg/store/index/Indexable.scala
@@ -21,36 +21,51 @@ trait TextIndexable[Type] extends Indexable[Type] { this: SqlColumnCommonRep[Typ
 
   override def createIndex(tableName: String) : Option[String] = {
     val sql = this.physColumns.map { phyCol =>
-      s"CREATE INDEX idx_${tableName}_$phyCol on $tableName USING BTREE ($phyCol text_pattern_ops);" +
-        s"CREATE INDEX idx_${tableName}_u_$phyCol on $tableName USING BTREE (upper($phyCol) text_pattern_ops)"
+      // nl for order by col asc nulls lasts and col desc nulls first
+      // nf for order by col asc nulls first and col desc nulls last
+      // text_pattern_ops for like 'prefix%'
+      s"""
+      CREATE INDEX idx_${tableName}_$phyCol on $tableName USING BTREE ($phyCol text_pattern_ops);
+      CREATE INDEX idx_${tableName}_u_$phyCol on $tableName USING BTREE (upper($phyCol) text_pattern_ops);
+      CREATE INDEX idx_${tableName}_nl_$phyCol on $tableName USING BTREE ($phyCol nulls last);
+      CREATE INDEX idx_${tableName}_nf_$phyCol on $tableName USING BTREE ($phyCol nulls first);
+      CREATE INDEX idx_${tableName}_unl_$phyCol on $tableName USING BTREE (upper($phyCol) nulls last);
+      CREATE INDEX idx_${tableName}_unf_$phyCol on $tableName USING BTREE (upper($phyCol) nulls first)"""
     }.mkString(";")
     Some(sql)
   }
-
 }
 
-trait NumberLikeIndexable[Type] extends Indexable[Type] { this: SqlColumnCommonRep[Type] =>
+trait BaseIndexable[Type] extends Indexable[Type] { this: SqlColumnCommonRep[Type] =>
 
   override def createIndex(tableName: String) : Option[String] = {
     val sql = this.physColumns.map { phyCol =>
-      s"CREATE INDEX idx_${tableName}_n_$phyCol on $tableName USING BTREE ($phyCol)"
+      s"""
+      CREATE INDEX idx_${tableName}_nl_$phyCol on $tableName USING BTREE ($phyCol nulls last);
+      CREATE INDEX idx_${tableName}_nf_$phyCol on $tableName USING BTREE ($phyCol nulls first)"""
     }.mkString(";")
     Some(sql)
   }
-
 }
+
+trait NumberLikeIndexable[Type] extends BaseIndexable[Type]  { this: SqlColumnCommonRep[Type] => }
+
+trait TimestampLikeIndexable[Type] extends BaseIndexable[Type]  { this: SqlColumnCommonRep[Type] => }
+
+trait BooleanIndexable[Type] extends BaseIndexable[Type] { this: SqlColumnCommonRep[Type] => }
+
 
 object SoQLIndexableRep {
 
   private val sqlRepFactories = Map[SoQLType, ColumnInfo[SoQLType] => SqlColumnRep[SoQLType, SoQLValue] with Indexable[SoQLType]](
-    SoQLID -> (ci => new IDRep(ci.physicalColumnBase)  with NoIndex[SoQLType] ), // TODO: Revisit index need
+    SoQLID -> (ci => new IDRep(ci.physicalColumnBase)  with NoIndex[SoQLType]), // Already indexed
     SoQLVersion -> (ci => new VersionRep(ci.physicalColumnBase) with NoIndex[SoQLType]), // TODO: Revisit index need
-    SoQLText -> (ci => new TextRep(ci.physicalColumnBase) with TextIndexable[SoQLType] ),
-    SoQLBoolean -> (ci => new BooleanRep(ci.physicalColumnBase) with NoIndex[SoQLType]), // TODO: Revisit index need
+    SoQLText -> (ci => new TextRep(ci.physicalColumnBase) with TextIndexable[SoQLType]),
+    SoQLBoolean -> (ci => new BooleanRep(ci.physicalColumnBase) with BooleanIndexable[SoQLType]),
     SoQLNumber -> (ci => new NumberLikeRep(SoQLNumber, _.asInstanceOf[SoQLNumber].value, SoQLNumber(_), ci.physicalColumnBase) with NumberLikeIndexable[SoQLType]),
     SoQLMoney -> (ci => new NumberLikeRep(SoQLNumber, _.asInstanceOf[SoQLMoney].value, SoQLMoney(_), ci.physicalColumnBase) with NumberLikeIndexable[SoQLType]),
-    SoQLFixedTimestamp -> (ci => new FixedTimestampRep(ci.physicalColumnBase) with NoIndex[SoQLType]), // TODO: Revisit index need
-    SoQLFloatingTimestamp -> (ci => new FloatingTimestampRep(ci.physicalColumnBase) with NoIndex[SoQLType]), // TODO: Revisit index need
+    SoQLFixedTimestamp -> (ci => new FixedTimestampRep(ci.physicalColumnBase) with TimestampLikeIndexable[SoQLType]),
+    SoQLFloatingTimestamp -> (ci => new FloatingTimestampRep(ci.physicalColumnBase) with TimestampLikeIndexable[SoQLType]),
     SoQLDate -> (ci => new DateRep(ci.physicalColumnBase) with NoIndex[SoQLType]), // TODO: Revisit index need
     SoQLTime -> (ci => new TimeRep(ci.physicalColumnBase) with NoIndex[SoQLType]), // TODO: Revisit index need
     SoQLLocation -> (ci => new LocationRep(ci.physicalColumnBase) with NoIndex[SoQLType]), // TODO: Revisit index need
