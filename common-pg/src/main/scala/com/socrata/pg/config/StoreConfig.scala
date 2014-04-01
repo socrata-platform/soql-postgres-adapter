b8aaa22eaa626a7e7b00c0214eee783db5f02fb6
Chi Tang <chitang@mac2.local>
2014-04-01 14:38:20 -0700

    Dependency change.
    Reviewed-by: randy.antler@socrata.com

--- common-pg/src/main/scala/com/socrata/pg/config/StoreConfig.scala
+++ common-pg/src/main/scala/com/socrata/pg/config/StoreConfig.scala
@@ -1,12 +1,12 @@
 package com.socrata.pg.config
 
-import com.typesafe.config.Config
+import com.typesafe.config.{ConfigUtil, Config}
 import com.socrata.thirdparty.typesafeconfig.ConfigClass
 import com.socrata.datacoordinator.common.DataSourceConfig
 
 class StoreConfig(config: Config, root: String) extends ConfigClass(config, root) {
 
-  protected override def path(key: String) = (if (root == "") root else root + ".") + key
+  protected override def path(key: String*) = if (root == "") ConfigUtil.joinPath(key: _*) else super.path(key: _*)
 
   val database = new DataSourceConfig(config, path("database"))
 
