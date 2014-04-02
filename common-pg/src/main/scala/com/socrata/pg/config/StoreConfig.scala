7015ea28faa02ed99332df95cd8945fc8a36ce00
Chi Tang <chitang@mac2.local>
2014-04-01 21:47:45 -0700

    store config did not load in staging.

--- common-pg/src/main/scala/com/socrata/pg/config/StoreConfig.scala
+++ common-pg/src/main/scala/com/socrata/pg/config/StoreConfig.scala
@@ -6,7 +6,9 @@ import com.socrata.datacoordinator.common.DataSourceConfig
 
 class StoreConfig(config: Config, root: String) extends ConfigClass(config, root) {
 
-  protected override def path(key: String*) = super.path(key: _*).stripPrefix(".") // handle blank root
+  private def path(key: String) = (if (root == "") root else root + ".") + key // handle blank root
+
+  override def getString(key: String): String = config.getString(path(key))
 
   val database = new DataSourceConfig(config, path("database"))
 
