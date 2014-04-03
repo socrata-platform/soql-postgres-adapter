1562a36269c11ffa390516e340a74978d3188e4a
Chi Tang <chitang@mac2.local>
2014-04-03 11:57:21 -0700

    Missing a file.

---
+++ common-pg/src/main/scala/com/socrata/pg/Version.scala
@@ -0,0 +1,12 @@
+package com.socrata.pg
+
+import com.rojoma.simplearm.util._
+import java.nio.charset.StandardCharsets
+
+object Version {
+
+  def apply(name: String) = for {
+    stream <- managed(getClass.getClassLoader.getResourceAsStream(s"$name.json"))
+    reader <- managed(new java.io.InputStreamReader(stream, StandardCharsets.UTF_8))
+  } yield com.rojoma.json.io.JsonReader.fromReader(reader)
+}
