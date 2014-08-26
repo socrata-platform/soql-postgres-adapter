ca6047a98ddee6ed47b0a8e6c7ca1a9d923e90d7
Chi Tang <chi.tang@socrata.com>
2014-08-26 08:36:04 -0700

    CR - add space after ":".

--- store-pg/src/main/scala/com/socrata/pg/store/events/WorkingCopyCreatedHandler.scala
+++ store-pg/src/main/scala/com/socrata/pg/store/events/WorkingCopyCreatedHandler.scala
@@ -1,23 +1,21 @@
 package com.socrata.pg.store.events
 
-import com.socrata.pg.store.{PGSecondaryLogger, PGSecondaryUniverse}
-import com.socrata.soql.types.{SoQLValue, SoQLType}
-import com.socrata.datacoordinator.truth.loader.SchemaLoader
 import com.socrata.datacoordinator.secondary.{CopyInfo => SecondaryCopyInfo, DatasetInfo => SecondaryDatasetInfo}
-import com.socrata.datacoordinator.truth.metadata.{CopyInfo => TruthCopyInfo, LifecycleStage}
-import com.rojoma.simplearm.util._
+import com.socrata.datacoordinator.truth.loader.SchemaLoader
+import com.socrata.datacoordinator.truth.metadata.{CopyInfo => TruthCopyInfo}
+import com.socrata.pg.store.{PGSecondaryLogger, PGSecondaryUniverse}
+import com.socrata.soql.types.{SoQLType, SoQLValue}
 
 /**
  * Handles WorkingCopyCreated Event
  */
 case class WorkingCopyCreatedHandler(pgu: PGSecondaryUniverse[SoQLType, SoQLValue],
                                      datasetId: Option[com.socrata.datacoordinator.id.DatasetId],
-                                     datasetInfo:SecondaryDatasetInfo,
-                                     copyInfo:SecondaryCopyInfo) {
-
+                                     datasetInfo: SecondaryDatasetInfo,
+                                     copyInfo: SecondaryCopyInfo) {
   copyInfo.copyNumber match {
     case 1 =>
-      val (copyInfoSecondary:TruthCopyInfo, sLoader) = createDataset(pgu, datasetInfo.localeName)
+      val (copyInfoSecondary: TruthCopyInfo, sLoader) = createDataset(pgu, datasetInfo.localeName)
       pgu.secondaryDatasetMapWriter.createInternalNameMapping(datasetInfo.internalName, copyInfoSecondary.datasetInfo.systemId)
     case copyNumBeyondOne =>
       pgu.datasetMapReader.datasetInfo(datasetId.get).map { truthDatasetInfo =>
@@ -33,7 +31,7 @@ case class WorkingCopyCreatedHandler(pgu: PGSecondaryUniverse[SoQLType, SoQLValu
       }
   }
 
-  private def createDataset(pgu: PGSecondaryUniverse[SoQLType, SoQLValue], locale:String):(TruthCopyInfo, SchemaLoader[SoQLType]) = {
+  private def createDataset(pgu: PGSecondaryUniverse[SoQLType, SoQLValue], locale:String): (TruthCopyInfo, SchemaLoader[SoQLType]) = {
     val copyInfo = pgu.datasetMapWriter.create(locale)
     val sLoader = pgu.schemaLoader(new PGSecondaryLogger[SoQLType, SoQLValue])
     // TODO figure out best way to avoid creating unused log and audit tables.
