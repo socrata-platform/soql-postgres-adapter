2cc1570a7d01b0ab33f2a9077346a9b160a8456a
Chi Tang <chi.tang@socrata.com>
2014-09-15 10:23:36 -0700

    Use copier instead of resync to copy pg table content.
    Reviewed-by: robert.macomber@socrata.com

--- store-pg/src/main/scala/com/socrata/pg/store/events/SecondaryEventHandler.scala
+++ store-pg/src/main/scala/com/socrata/pg/store/events/SecondaryEventHandler.scala
@@ -1,8 +1,10 @@
 package com.socrata.pg.store.events
 
-import com.socrata.datacoordinator.truth.metadata.{CopyInfo => TruthCopyInfo, DatasetInfo => TruthDatasetInfo}
+import com.socrata.datacoordinator.truth.metadata.{CopyInfo => TruthCopyInfo, DatasetInfo => TruthDatasetInfo, DatasetCopyContext}
+import com.socrata.datacoordinator.secondary.ResyncSecondaryException
 import com.socrata.pg.store.PGSecondaryUniverse
 import com.socrata.soql.types.{SoQLType, SoQLValue}
+
 
 case class CopyDroppedHandler(pgu: PGSecondaryUniverse[SoQLType, SoQLValue], copyInfo: TruthCopyInfo) {
   pgu.datasetMapWriter.dropCopy(copyInfo)
@@ -23,4 +25,17 @@ case class WorkingCopyDroppedHandler(pgu: PGSecondaryUniverse[SoQLType, SoQLValu
 
 case class WorkingCopyPublishedHandler(pgu: PGSecondaryUniverse[SoQLType, SoQLValue], truthCopyInfo: TruthCopyInfo) {
   pgu.datasetMapWriter.publish(truthCopyInfo)
+}
+
+case class DataCopiedHandler(pgu: PGSecondaryUniverse[SoQLType, SoQLValue], truthDatasetInfo: TruthDatasetInfo, truthCopyInfo: TruthCopyInfo) {
+  pgu.datasetMapReader.published(truthDatasetInfo) match {
+    case Some(publishedCopy) =>
+      val copier = pgu.datasetContentsCopier(pgu.logger(truthDatasetInfo, "_no_user")) // TODO: fill in proper user
+      val schema = pgu.datasetMapReader.schema(truthCopyInfo)
+      val copyCtx = new DatasetCopyContext[SoQLType](truthCopyInfo, schema)
+      copier.copy(publishedCopy, copyCtx)
+    case None =>
+      val msg = s"DataCopy triggers resync dataset ${truthDatasetInfo.systemId} copy-${truthCopyInfo.copyNumber} data-ver-${truthCopyInfo.dataVersion}"
+      throw new ResyncSecondaryException(msg)
+  }
 }
