9f97104e6ee2a4171e528d747123af2d2c64398d
Chi Tang <chi.tang@socrata.com>
2014-12-30 09:42:59 -0800

    case function

---
+++ soql-server-pg/src/test/scala/com/socrata/pg/server/SoQLCaseFunctionTest.scala
@@ -0,0 +1,43 @@
+package com.socrata.pg.server
+
+class SoQLCaseFunctionsTest extends SoQLTest {
+
+  test("or inside case") {
+    compareSoqlResult("""
+      SELECT case(class = 'EN-A' or class = 'DHV-1', 'Beginner',
+                  class = 'EN-B', 'Intermediate',
+                  class = 'EN-C', 'Advanced',
+                  true, 'Unknown') as category,
+             class,
+             name
+       ORDER By category, name
+       LIMIT 10""",
+      "where-case-or.json")
+  }
+
+  test("two cases return the same value") {
+    compareSoqlResult(
+      """
+        SELECT case(class = 'EN-A', 'Beginner',
+                    class = 'DHV-1', 'Beginner',
+                    class = 'EN-B', 'Intermediate',
+                    class = 'EN-C', 'Advanced') as category,
+               class,
+               name
+         ORDER By category, name
+         LIMIT 10""",
+      "where-case-or.json")
+  }
+
+  test("case group") {
+    compareSoqlResult(
+      """
+        SELECT case(class = 'EN-A', 'Beginner',
+                    class = 'DHV-1', 'Beginner',
+                    class = 'EN-B', 'Intermediate',
+                    class = 'EN-C', 'Advanced') as category,
+               count(:id) as count
+         GROUP By category""",
+      "group-case.json")
+  }
+}
