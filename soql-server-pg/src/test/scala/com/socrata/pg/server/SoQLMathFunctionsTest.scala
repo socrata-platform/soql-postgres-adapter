3e8dd0506909c55705abb8c596bb71d128b18b2d
Chi Tang <chi.tang@socrata.com>
2015-03-11 14:18:04 -0700

    Add a few more test cases.

--- soql-server-pg/src/test/scala/com/socrata/pg/server/SoQLMathFunctionsTest.scala
+++ soql-server-pg/src/test/scala/com/socrata/pg/server/SoQLMathFunctionsTest.scala
@@ -3,7 +3,12 @@ package com.socrata.pg.server
 class SoQLMathFunctionsTest extends SoQLTest {
 
   test("signed_magnitude_10(x)") {
-    compareSoqlResult("select name, line_length, signed_magnitude_10(line_length) order by name",
-                      "select-signed_magnitude_10.json")
+    compareSoqlResult(
+      """select name, line_length, signed_magnitude_10(line_length),
+                signed_magnitude_10(0) as z,
+                signed_magnitude_10(9.9) as p,
+                signed_magnitude_10(-9.8) as n
+          order by name""",
+      "select-signed_magnitude_10.json")
   }
 }
