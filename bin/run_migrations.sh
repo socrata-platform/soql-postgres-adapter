4b3e8e5e7f90914f0dc3d003ad6b126a48e0d11e
Kaida Masaki <kaida.masaki@socrata.com>
2015-03-20 16:20:58 -0700

    Fix onramp path

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 BASEDIR=$(dirname $0)/..
-CONFIG=${SODA_CONFIG:-$BASEDIR/../onramp/services/soda2.conf}
+CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
 JARFILE=$BASEDIR/store-pg/target/scala-2.10/store-pg-assembly-*.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt assembly
