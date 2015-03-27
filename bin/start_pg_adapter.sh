b7f13f2144a8883ae9ace6edf4843a00f35e7101
Evan Chan <evan.chan@socrata.com>
2015-03-27 10:40:18 -0700

    Fix start scripts

--- bin/start_pg_adapter.sh
+++ bin/start_pg_adapter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Starts the soql-pg-adapter service.
 BASEDIR=$(dirname $0)/..
-CONFIG=${SODA_CONFIG:-$BASEDIR/../onramp/services/soda2.conf}
+CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
 JARFILE=$BASEDIR/soql-server-pg/target/scala-2.10/soql-server-pg-assembly-*.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt assembly
