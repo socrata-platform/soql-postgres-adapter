4d965f1decdd8015ebbe431f051ed33b101a408b
Kaida Masaki <kaida.masaki@socrata.com>
2015-10-07 14:36:47 -0700

    Handle multiple assembly jars

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-BASEDIR=$(dirname $0)/..
+BASEDIR=$(dirname "$0")/..
 CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
-JARFILE=$BASEDIR/store-pg/target/scala-2.10/store-pg-assembly-*.jar
-if [ ! -e $JARFILE ]; then
-  cd $BASEDIR && sbt assembly
+
+JARS=( $BASEDIR/store-pg/target/scala-*/store-pg-assembly-*.jar )
+# shellcheck disable=SC2012
+JARFILE=$(ls -t "${JARS[@]}" | head -n 1)
+
+if [ ! -e "$JARFILE" ]; then
+    cd "$BASEDIR" && sbt assembly
+    JARS=( $BASEDIR/store-pg/target/scala-*/store-pg-assembly-*.jar )
+    # shellcheck disable=SC2012
+    JARFILE=$(ls -t "${JARS[@]}" | head -n 1)
 fi
-java -Dconfig.file=$CONFIG -cp $JARFILE com.socrata.pg.store.MigrateSchema Migrate
+java -Dconfig.file="$CONFIG" -cp "$JARFILE" com.socrata.pg.store.MigrateSchema Migrate
