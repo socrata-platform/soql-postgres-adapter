fae77228e2b2d1cce1ed1130d267c66c4e27951d
Nathalie Kaligirwa <nathalie.kaligirwa@socrata.com>
2016-03-21 14:05:02 -0700

    EN-3726: function to validate geometries

--- common-pg/src/main/scala/com/socrata/pg/soql/SqlFunctionsGeometry.scala
+++ common-pg/src/main/scala/com/socrata/pg/soql/SqlFunctionsGeometry.scala
@@ -6,7 +6,7 @@ import com.socrata.pg.soql.SqlFunctions._
 import com.socrata.pg.soql.Sqlizer._
 import com.socrata.soql.functions.Function
 import com.socrata.soql.functions.SoQLFunctions._
-import com.socrata.soql.types.{SoQLMultiLine, SoQLMultiPolygon, SoQLValue, SoQLType}
+import com.socrata.soql.types.{SoQLPolygon, SoQLMultiLine, SoQLMultiPolygon, SoQLValue, SoQLType}
 
 // scalastyle:off magic.number multiple.string.literals
 trait SqlFunctionsGeometry {
@@ -37,6 +37,8 @@ trait SqlFunctionsGeometry {
     ConvexHull -> formatCall("ST_Multi(ST_ConvexHull(ST_Union(%s)))"),
     Intersects -> formatCall("ST_Intersects(%s, %s)") _,
     DistanceInMeters -> formatCall("ST_Distance(%s::geography, %s::geography)") _,
+    ValidMultiPolygon -> formatValidate("ST_MakeValid(%s)") _,
+    CuratedRegionTest -> formatCall("ST_isValid(%s)")_,
     NumberOfPoints -> formatCall("ST_NPoints(%s)") _,
     Simplify -> formatSimplify("ST_Simplify(%s, %s)") _,
     SimplifyPreserveTopology -> formatSimplify("ST_SimplifyPreserveTopology(%s, %s)") _,
@@ -64,6 +66,24 @@ trait SqlFunctionsGeometry {
         result
     }
   }
+
+  private def formatValidate(template: String , paramPosition: Option[Seq[Int]] = None)
+                            (fn: FunCall,
+                             rep: Map[UserColumnId, SqlColumnRep[SoQLType, SoQLValue]],
+                             typeRep: Map[SoQLType, SqlColumnRep[SoQLType, SoQLValue]],
+                             setParams: Seq[SetParam],
+                             ctx: Sqlizer.Context,
+                             escape: Escape): ParametricSql = {
+    val result@ParametricSql(Seq(sql), params) =
+      formatCall(template, paramPosition = paramPosition)(fn, rep, typeRep, setParams, ctx, escape)
+    fn.parameters.head.typ match {
+      case SoQLPolygon =>
+        ParametricSql(Seq("ST_Multi(%s)".format(sql)), params)
+      case _ =>
+        result
+    }
+  }
+
 
   private def isEmpty =
     formatCall("ST_IsEmpty(%s) or %s is null", paramPosition = Some(Seq(0, 0))) _
