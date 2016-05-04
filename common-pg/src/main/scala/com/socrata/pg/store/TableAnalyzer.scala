16c455743f42481b39078218580c16a238a7d8ec
Robert J. Macomber <robert.macomber@socrata.com>
2016-05-03 21:41:37 -0700

    Meeeh

--- common-pg/src/main/scala/com/socrata/pg/store/TableAnalyzer.scala
+++ common-pg/src/main/scala/com/socrata/pg/store/TableAnalyzer.scala
@@ -3,5 +3,5 @@ package com.socrata.pg.store
 import com.socrata.datacoordinator.truth.metadata.CopyInfo
 
 trait TableAnalyzer {
-  def analyze(copy: CopyInfo)
+  def analyze(copy: CopyInfo): Unit
 }
