9ebdef1a8c62a00f56f6ef167edbd8c1fd209c19
Megan Schneider <megan.schneider@socrata.com>
2016-11-07 10:37:59 -0800

    fix migrations script to point to correct config (#195)

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")/..
-CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
+CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/pg-secondary.conf}
 
 JARS=( $BASEDIR/store-pg/target/scala-2.10/store-pg-assembly-*.jar )
 # shellcheck disable=SC2012
