18715c4d1f361159a56921905b4332b965827299
Chi Tang <chi.tang@socrata.com>
2017-01-03 09:37:36 -0800

    EN-12742 Add capability to disable a secondary instance from reading a dataset (#199)
    update dataset_internal_name_map set disabled to now()
    
    rebuild

--- common-pg/src/main/scala/com/socrata/pg/store/PGSecondaryDatasetMapReader.scala
+++ common-pg/src/main/scala/com/socrata/pg/store/PGSecondaryDatasetMapReader.scala
@@ -6,16 +6,20 @@ import com.socrata.datacoordinator.id.DatasetId
 
 class PGSecondaryDatasetMapReader(val conn: Connection) {
   val idFromName =
-    """SELECT dataset_system_id
+    """SELECT dataset_system_id, disabled
       |  FROM dataset_internal_name_map
       | WHERE dataset_internal_name = ?
     """.stripMargin
-  def datasetIdForInternalName(datasetInternalName: String): Option[DatasetId] = {
+  def datasetIdForInternalName(datasetInternalName: String, checkDisabled: Boolean = false): Option[DatasetId] = {
     using(conn.prepareStatement(idFromName)) { stmt =>
       stmt.setString(1, datasetInternalName)
       using(stmt.executeQuery()) { rs =>
         if (rs.next()) {
-          Option(new DatasetId(rs.getLong("dataset_system_id")))
+          if (checkDisabled && rs.getDate("disabled") != null) {
+            None
+          } else {
+            Option(new DatasetId(rs.getLong("dataset_system_id")))
+          }
         } else {
           None
         }
