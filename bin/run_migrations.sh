84e92a427f67f3e44e33cf3b0ade278226ee2b64
Kaida Masaki <kaida.masaki@socrata.com>
2017-03-03 16:16:10 -0800

    Register secondary watcher when running migrations

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -4,6 +4,8 @@ set -e
 REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BINDIR=$(dirname "$REALPATH")
 
+psql datacoordinator -c "insert into secondary_stores_config (store_id, next_run_time, interval_in_seconds) values ('pg', now(), 5) on conflict do nothing"
+
 CONFIG=${SODA_CONFIG:-/etc/pg-secondary.conf}
 
 JARFILE=$("$BINDIR"/build.sh "$@" | grep '^store-pg: ' | sed 's/^store-pg: //')
