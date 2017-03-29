759952b4513e94cfe98e20b1dee06b753646531e
Chi Tang <chi.tang@socrata.com>
2017-03-29 14:56:09 -0700

    EN-9028 Don’t use postgres 9.6 upsert yet. (#212)
    * EN-9028 Don’t use postgres 9.6 upsert yet.
    
    * Add group name ‘read’

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -4,7 +4,7 @@ set -e
 REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BINDIR=$(dirname "$REALPATH")
 
-psql datacoordinator -c "insert into secondary_stores_config (store_id, next_run_time, interval_in_seconds) values ('pg', now(), 5) on conflict do nothing"
+psql datacoordinator -c "insert into secondary_stores_config (store_id, next_run_time, interval_in_seconds, group_name) SELECT * FROM (SELECT 'pg'::text as store_id, now(), 5, 'read'::text) t WHERE store_id NOT IN (SELECT store_id FROM secondary_stores_config)"
 
 CONFIG=${SODA_CONFIG:-/etc/pg-secondary.conf}
 
