fe976985437021682b30d1d0e696a502fa7f7c17
Chi Tang <chi.tang@socrata.com>
2017-05-23 16:53:42 -0700

    EN-16378 Handle grouping derived views. (#228)
    * EN-16378 Handle grouping derived views.
    
    * Add test and comment.
    
    Retest please

--- common-pg/src/main/scala/com/socrata/pg/soql/ExprSqlizer.scala
+++ common-pg/src/main/scala/com/socrata/pg/soql/ExprSqlizer.scala
@@ -125,7 +125,8 @@ object ColumnRefSqlizer extends Sqlizer[ColumnRef[UserColumnId, SoQLType]] {
           escape: Escape): ParametricSql = {
     val tableMap = ctx(TableAliasMap).asInstanceOf[Map[String, String]]
     reps.get(QualifiedUserColumnId(expr.qualifier, expr.column)) match {
-      case Some(rep) if ctx(InnermostSoql) == true => // scalastyle:off simplify.boolean.expression
+      case Some(rep) if ctx(InnermostSoql) == true || expr.qualifier.nonEmpty => // scalastyle:off simplify.boolean.expression
+        // fields from the innermost soql and fields from joins in following chained soqls interact with physical tables.
         if (complexTypes.contains(expr.typ) &&
             ctx.get(SoqlPart).exists(_ == SoqlSelect) &&
             ctx.get(RootExpr).exists(_ == expr)) {
--- common-pg/src/test/scala/com/socrata/pg/soql/SqlizerJoinTest.scala
+++ common-pg/src/test/scala/com/socrata/pg/soql/SqlizerJoinTest.scala
@@ -25,4 +25,12 @@ class SqlizerJoinTest  extends SqlizerTest {
     sql should be ("SELECT (count(*)) FROM (SELECT t1.case_number as \"case_number\",t1.primary_type as \"primary_type\",_y.avg_temperature as \"avg_temperature\" FROM t1 JOIN t3 as _y ON (_y.year = t1.year)) AS x1")
     setParams.length should be (0)
   }
+
+  test("group and then join with qualified fields from join") {
+    val soql = "select primary_type, count(case_number), max(year) as year group by primary_type |> select primary_type, year, @y.avg_temperature join @year as y on @y.year = year and year = 2000"
+    val ParametricSql(Seq(sql), setParams) = sqlize(soql, CaseSensitive)
+    sql should be ("SELECT \"primary_type\",\"year\",_y.avg_temperature FROM (SELECT t1.primary_type as \"primary_type\",(count(t1.case_number)) as \"count_case_number\",(max(t1.year)) as \"year\" FROM t1 GROUP BY t1.primary_type) AS x1 JOIN t3 as _y ON ((_y.year = \"year\") and (\"year\" = ?))")
+    val params = setParams.map { (setParam) => setParam(None, 0).get }
+    params should be (Seq(2000))
+  }
 }
