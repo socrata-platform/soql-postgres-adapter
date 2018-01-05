da197b5ffb58918470be0a36f1316aeea08c3567
Michael Brown <michael.brown@socrata.com>
2018-01-05 10:44:52 -0800

    add back commented line

--- bin/build_soql_server_pg_docker.sh
+++ bin/build_soql_server_pg_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-#sbt soql-server-pg/assembly
+sbt soql-server-pg/assembly
 jarfile="$(ls -t soql-server-pg/target/scala-2.*/soql-server-pg-assembly-*.jar | head -1)"
 cp "$jarfile" soql-server-pg/docker/soql-server-pg-assembly.jar
 docker build --pull -t soql-server-pg soql-server-pg/docker
