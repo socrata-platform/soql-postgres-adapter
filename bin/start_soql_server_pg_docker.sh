6d4ed2fbd20f37bcd21319f67cbe35c57bb2072a
Alexa Rust <aerust@uw.edu>
2018-02-01 15:59:35 -0800

    EN-21594: Rework dev config to match style (#260)
    Rework new dev config files to match the same style and layout of the
    rest of the soda NBE services. Also update README with correct SBT
    commands. This is to remove the `/etc/soda2.conf`.

--- bin/start_soql_server_pg_docker.sh
+++ bin/start_soql_server_pg_docker.sh
@@ -9,7 +9,7 @@ fi
 
 local_config_dir="$(dirname "$(realpath "$0")")/../configs"
 docker run \
-  -e SERVER_CONFIG="configs/soql-postgres-adapter.conf" \
+  -e SERVER_CONFIG="configs/application.conf" \
   -v "$local_config_dir":/srv/soql-server-pg/configs \
   -p 6090:6090 \
   -p 6091:6091 \
