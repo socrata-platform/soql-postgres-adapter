106198ddd17133b77b29744aca60bcd2a441bb33
Alexa Rust <alexa.rust@socrata.com>
2018-03-07 11:49:45 -0800

    SPARTA: Starts scripts for solo for phonetic SWs
    Start scripts for solo for running secondary-watcher-pgs
    for the phonetic truth cluster "alpha", "bravo", "charlie"

---
+++ bin/start_secondary_watcher_pg_docker_alpha.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+image="$1"
+if [ -z "$image" ]; then
+  echo "Please pass in the image to run as the first argument to this script!"
+  exit 1
+fi
+
+local_config_dir="$(dirname "$(realpath "$0")")/../configs"
+docker run \
+  -e SERVER_CONFIG="configs/application-alpha.conf" \
+  -v "$local_config_dir":/srv/secondary-watcher/configs \
+  -p 6300:6300 \
+  -d -t "$image"
