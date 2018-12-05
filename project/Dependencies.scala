aa93304f43f9b116e48b3aa5adb3b81aeafaa01c
Chi Tang <chi.tang@socrata.com>
2018-12-04 16:44:03 -0800

    EN-28816 Fix chained soql merge bug

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val socrataCuratorUtils = "1.0.1"
     val socrataThirdPartyUtils = "4.0.1"
     val socrataHttpCuratorBroker = "3.3.0"
-    val soqlStdlib = "2.11.0"
+    val soqlStdlib = "2.11.3"
     val typesafeConfig = "1.0.0"
     val dataCoordinator = "3.4.30"
     val typesafeScalaLogging = "1.1.0"
