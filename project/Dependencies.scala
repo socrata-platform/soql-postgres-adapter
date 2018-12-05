de37f6b3b8433b2f4c4a387b06fbbba66d8abe01
Chi Tang <chi.tang@socrata.com>
2018-12-05 13:15:37 -0800

    Bump data-coordinator version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val socrataHttpCuratorBroker = "3.3.0"
     val soqlStdlib = "2.11.3"
     val typesafeConfig = "1.0.0"
-    val dataCoordinator = "3.4.30"
+    val dataCoordinator = "3.4.34"
     val typesafeScalaLogging = "1.1.0"
     val rojomaJson = "3.9.1"
     val metricsJetty = "3.1.0"
