157b24ecc400808932051dddb58040ecb5379062
Chi Tang <chi.tang@socrata.com>
2019-01-05 12:42:48 -0800

    minor refactor

--- common-pg/src/main/scala/com/socrata/pg/soql/SoQLAnalysisSqlizer.scala
+++ common-pg/src/main/scala/com/socrata/pg/soql/SoQLAnalysisSqlizer.scala
@@ -75,10 +75,11 @@ object SoQLAnalysisSqlizer extends Sqlizer[AnalysisTarget] {
     val (result, _) = analyses.tail.foldLeft((firstSql, analyses.head)) { (acc, ana) =>
       subTableIdx += 1
       val (subParametricSql, prevAna) = acc
-      val subTableName = "(%s) AS x%d".format(subParametricSql.sql.head, subTableIdx)
+      val chainedTableAlias = s"x${subTableIdx}"
+      val subTableName = "(%s) AS %s".format(subParametricSql.sql.head, chainedTableAlias)
       val tableNamesSubTableNameReplace = tableNames + (TableName.PrimaryTable -> subTableName)
       val primaryTableAlias =
-        if (ana == lastAna && ana.joins.nonEmpty) Map((PrimaryTableAlias -> s"x${subTableIdx}"))
+        if (ana == lastAna && ana.joins.nonEmpty) Map((PrimaryTableAlias -> chainedTableAlias))
         else Map.empty
       val subCtx = ctx + (OutermostSoql -> outermostSoql(ana, analyses)) +
                          (InnermostSoql -> innermostSoql(ana, analyses)) -
