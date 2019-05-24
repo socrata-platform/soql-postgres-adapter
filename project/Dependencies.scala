1c887ad8f702b0c40ebd757b9820a6242a1b0165
Chi Tang <chitang@SEAPPLTCTANG.local>
2019-05-24 00:03:08 -0700

    Bump references version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val socrataHttpCuratorBroker = "3.11.4"
     val soqlStdlib = "2.11.11"
     val typesafeConfig = "1.0.0"
-    val dataCoordinator = "3.5.3"
+    val dataCoordinator = "3.5.4"
     val typesafeScalaLogging = "1.1.0"
     val rojomaJson = "3.9.1"
     val metricsJetty = "3.1.0"
