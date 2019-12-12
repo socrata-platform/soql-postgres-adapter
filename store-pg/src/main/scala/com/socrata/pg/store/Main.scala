30246cc3cccde947455e7b116171dc738aefe8d9
Robert J. Macomber <robert.macomber@socrata.com>
2019-12-12 11:24:54 -0800

    Rearrange migrations so server doesn't need to depend on store
    Puts the common code in common but keeps the actual invocation living
    in the soql-server-pg

--- store-pg/src/main/scala/com/socrata/pg/store/Main.scala
+++ store-pg/src/main/scala/com/socrata/pg/store/Main.scala
@@ -3,9 +3,5 @@ package com.socrata.pg.store
 import com.socrata.datacoordinator.secondary.SecondaryWatcherApp
 
 object Main extends App {
-  if(args.headOption == Some("--migrate")) {
-    MigrateSchema(args.drop(1))
-  } else {
-    SecondaryWatcherApp(new PGSecondary(_))
-  }
+  SecondaryWatcherApp(new PGSecondary(_))
 }
