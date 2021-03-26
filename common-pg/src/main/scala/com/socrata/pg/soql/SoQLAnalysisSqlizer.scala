d4fcfb353470e9d456b9f2940e536d8c87a97b7a
Chi Tang <chi.tang@socrata.com>
2021-03-26 09:57:05 -0700

    Revert "Revert "Lateral joins (#385)"" (#386)
    This reverts commit f3721cf3663b6b73a6fd4667a39cfd528db76bd9.

--- common-pg/src/main/scala/com/socrata/pg/soql/SoQLAnalysisSqlizer.scala
+++ common-pg/src/main/scala/com/socrata/pg/soql/SoQLAnalysisSqlizer.scala
@@ -321,7 +321,8 @@ trait SoQLAnalysisSqlizer {
 
       val joinConditionParamSql = Sqlizer.sql(join.on)(repMinusComplexJoinTable, typeRep, joinOnParams, ctxSelectWithJoins + (SoqlPart -> SoqlJoin), escape)
       val joinCondition = joinConditionParamSql.sql.mkString(" ")
-      (sqls :+ s" ${join.typ.toString} $tableName ON $joinCondition", joinConditionParamSql.setParams)
+      val lateral = if (join.lateral) "LATERAL " else ""
+      (sqls :+ s" ${join.typ.toString} ${lateral}$tableName ON $joinCondition", joinConditionParamSql.setParams)
     }
 
     val setParamsSelectJoin = setParamsSelect ++ setParamsJoin
