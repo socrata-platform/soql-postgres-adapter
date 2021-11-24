bfeee8da4fec6a0767d8eec55e8819d8d1b9e747
Chi <chi.tang@socrata.com>
2021-11-24 14:15:35 -0800

    Catch geometry errors (400) previously (500)

--- soql-server-pg/src/main/scala/com/socrata/pg/server/QueryServer.scala
+++ soql-server-pg/src/main/scala/com/socrata/pg/server/QueryServer.scala
@@ -781,10 +781,11 @@ object QueryServer extends DynamicPortMap {
   class QueryRuntimeError(val error: SQLException) extends QueryError(error.getMessage)
   object QueryRuntimeError {
     val validErrorCodes = Set(
-      "22003", // value overflows numeric format, 1000000000000 ^ 1000000000000000000
-      "22012", // divide by zero, 1/0
-      "22008", // timestamp out of range, timestamp - 'P500000Y'
-      "22P02" // invalid input syntax for type boolean|numeric:, 'tr2ue'::boolean 'tr2ue'::number
+      "22003",    // value overflows numeric format, 1000000000000 ^ 1000000000000000000
+      "22012",    // divide by zero, 1/0
+      "22008",    // timestamp out of range, timestamp - 'P500000Y'
+      "22P02",    // invalid input syntax for type boolean|numeric:, 'tr2ue'::boolean 'tr2ue'::number
+      "XX000"     // invalid geometry, POINT(1,2)::point -- no comma between lon lat, POLYGON((0 0,1 1,1 0))::polygon
     )
 
     def apply(error: SQLException, timeout: Option[Duration] = None): Option[QueryResult] = {
