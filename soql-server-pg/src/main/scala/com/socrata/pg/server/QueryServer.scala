b98652f02da74e6c294613b8f13306f2f69e5017
Chi <chi.tang@socrata.com>
2021-11-24 14:17:51 -0800

    Update usages of deprecated function

--- soql-server-pg/src/main/scala/com/socrata/pg/server/QueryServer.scala
+++ soql-server-pg/src/main/scala/com/socrata/pg/server/QueryServer.scala
@@ -428,7 +428,7 @@ class QueryServer(val dsInfo: DSInfo, val caseSensitivity: CaseSensitivity, val 
       for(readCtx <- pgu.datasetReader.openDataset(latestCopy)) {
         val baseSchema: ColumnIdMap[ColumnInfo[SoQLType]] = readCtx.schema
         val systemToUserColumnMap = SchemaUtil.systemToUserColumnMap(readCtx.schema)
-        val qrySchema = querySchema(pgu, analyses.outputSchemaLeaf, latestCopy)
+        val qrySchema = querySchema(pgu, analyses.outputSchema.leaf, latestCopy)
         val qryReps = qrySchema.mapValues(pgu.commonSupport.repFor)
         val querier = this.readerWithQuery(pgu.conn, pgu, readCtx.copyCtx, baseSchema, rollupName)
         val sqlReps = querier.getSqlReps(readCtx.copyInfo.dataTableName, systemToUserColumnMap)
