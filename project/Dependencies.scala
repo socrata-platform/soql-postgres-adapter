4f77a3ac9465750a949755badb810218996f6838
Chi Tang <chi.tang@socrata.com>
2022-06-14 11:34:15 -0700

    Bump soql-reference dependent version (#484)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val socrataCuratorUtils = "1.2.0"
     val socrataThirdPartyUtils = "5.0.0"
     val socrataHttpCuratorBroker = "3.13.4"
-    val soqlStdlib = "4.6.0"
+    val soqlStdlib = "4.7.0"
     val typesafeConfig = "1.0.0"
     val dataCoordinator = "4.0.2"
     val typesafeScalaLogging = "3.9.2"
