d71ed8c9ca77bdcfe246a1171e66c56dca44568c
Chi Tang <chi.tang@socrata.com>
2022-06-20 09:02:28 -0700

    EN-53459: Select Distinct on (field) field |> Select count(*) fails to sqlize (#487)

--- common-pg/src/main/scala/com/socrata/pg/soql/Sqlizer.scala
+++ common-pg/src/main/scala/com/socrata/pg/soql/Sqlizer.scala
@@ -171,6 +171,7 @@ object SqlizerContext extends Enumeration {
   val SoqlHaving = Value("having")
   val SoqlOrder = Value("order")
   val SoqlSearch = Value("search")
+  val SoqlDistinct = Value("distinct")
   // Normally geometry is converted to text when used in select.
   // But if the sql is used for table insert like rollup, we do not want geometry converted to text.
   val LeaveGeomAsIs = Value("leave-geom-as-is")
