52df8682000d23af076eb1e0650a65a7efa03d79
zachary lefevre <zachlefevre@gmail.com>
2023-06-21 17:03:55 -0500

    small changes

--- common-pg/src/main/scala/com/socrata/util/Citus.scala
+++ common-pg/src/main/scala/com/socrata/util/Citus.scala
@@ -21,101 +21,92 @@ object Citus {
 
   object MaybeDistribute {
 
-    def sql(conn: Connection, resourceNameOptional: Option[String], tableName: String): Option[String] = {
+    def sql(conn: Connection, resourceNameOptional: Option[String], tableName: String): Option[String] =
       resourceNameOptional.flatMap { resourceName =>
         sql(conn, new ResourceName(resourceName), TableName(tableName))
       }
-    }
 
-    def sql(conn: Connection, resourceName: ResourceName, tableName: TableName): Option[String] = {
+
+    def sql(conn: Connection, resourceName: ResourceName, tableName: TableName): Option[String] =
       if (shouldUseCitusDistribution) {
         generate(conn, resourceName, tableName) match {
-          case Left(x@Some(_)) => x
-          case Left(None) => log.info("No distribution key configured for dataset '%s'", resourceName.name)
+          case Right(x@Some(_)) => x
+          case Right(None) =>
+            log.info("No distribution key configured for dataset '%s'", resourceName.name)
             None
-          case Right(e) => log.error("Error while determining citus distribution key for dataset '%s'".format(resourceName.name), e)
+          case Left(e) =>
+            log.error("Error while determining citus distribution key for dataset '%s'".format(resourceName.name), e)
             None
         }
       } else {
         None
       }
-    }
 
-    def shouldUseCitusDistribution: Boolean = {
-      isCitus
-    }
 
-    def isCitus: Boolean = {
-      sys.env.get(IS_CITUS).exists { raw => raw.toBoolean }
-    }
+    def shouldUseCitusDistribution: Boolean = isCitus
 
-    private def generate(conn: Connection, resourceName: ResourceName, tableName: TableName): Either[Option[String], SQLException] = {
+
+    def isCitus: Boolean = sys.env.get(IS_CITUS).exists { raw => raw.toBoolean }
+
+    private def generate(conn: Connection, resourceName: ResourceName, tableName: TableName): Either[SQLException, Option[String]] =
       getDistributionColumnOfDataset(conn, resourceName) match {
-        case Left(Some(columnName)) => Left(Some("select create_distributed_table('%s', '%s');".format(tableName, columnName)))
-        case x@_ => x
+        case Right(Some(columnName)) => Right(Some("select create_distributed_table('%s', '%s');".format(tableName, columnName)))
+        case other => other
       }
-    }
 
-    private def getDistributionColumnOfDataset(conn: Connection, resourceName: ResourceName): Either[Option[String], SQLException] = {
+    private def getDistributionColumnOfDataset(conn: Connection, resourceName: ResourceName): Either[SQLException, Option[String]] =
       using(conn.prepareStatement("select column_name from citus_dataset_distribution_map where resource_name=?")) { stmt =>
         stmt.setString(1, resourceName.name)
         extractCitusDistributionColumnName(stmt.executeQuery())
       }
-    }
 
-    private def extractCitusDistributionColumnName(rs: ResultSet): Either[Option[String], SQLException] = {
+    private def extractCitusDistributionColumnName(rs: ResultSet): Either[SQLException, Option[String]] =
       try {
         if (rs.next()) {
-          Left(Some(rs.getString("column_name")))
+          Right(Some(rs.getString("column_name")))
         } else {
-          Left(None)
+          Right(None)
         }
       } catch {
-        case e: SQLException => Right(e)
+        case e: SQLException => Left(e)
       }
-    }
 
-    def sql(conn: Connection, resourceNameOptional: Option[String], tableName: String, rollupName: RollupName): Option[String] = {
+    def sql(conn: Connection, resourceNameOptional: Option[String], tableName: String, rollupName: RollupName): Option[String] =
       resourceNameOptional.flatMap { resourceName =>
         sql(conn, new ResourceName(resourceName), TableName(tableName), rollupName)
       }
-    }
 
-    def sql(conn: Connection, resourceName: ResourceName, tableName: TableName, rollupName: RollupName): Option[String] = {
+    def sql(conn: Connection, resourceName: ResourceName, tableName: TableName, rollupName: RollupName): Option[String] =
       if (shouldUseCitusDistribution) {
         generate(conn, resourceName, tableName, rollupName) match {
-          case Left(x@Some(_)) => x
-          case Left(None) => log.info("No distribution key configured for dataset '%s', rollup '%s'".format(resourceName.name, rollupName.underlying))
+          case Right(x@Some(_)) => x
+          case Right(None) =>
+            log.info("No distribution key configured for dataset '%s', rollup '%s'".format(resourceName.name, rollupName.underlying))
             None
-          case Right(e) => log.error("Error while determining citus distribution key for dataset '%s', rollup '%s'".format(resourceName.name, rollupName.underlying), e)
+          case Left(e) =>
+            log.error("Error while determining citus distribution key for dataset '%s', rollup '%s'".format(resourceName.name, rollupName.underlying), e)
             None
         }
       } else {
         None
       }
-    }
 
-    private def generate(conn: Connection, resourceName: ResourceName, tableName: TableName, rollupName: RollupName): Either[Option[String], SQLException] = {
+    private def generate(conn: Connection, resourceName: ResourceName, tableName: TableName, rollupName: RollupName): Either[SQLException, Option[String]] =
       getDistributionColumnOfRollup(conn, resourceName, rollupName) match {
-        case Left(Some(columnName)) => Left(Some("select create_distributed_table('%s', '%s');".format(tableName, columnName)))
-        case x@_ => x
+        case Right(Some(columnName)) => Right(Some("select create_distributed_table('%s', '%s');".format(tableName, columnName)))
+        case other => other
       }
-    }
 
-    private def getDistributionColumnOfRollup(conn: Connection, resourceName: ResourceName, rollupName: RollupName): Either[Option[String], SQLException] = {
+    private def getDistributionColumnOfRollup(conn: Connection, resourceName: ResourceName, rollupName: RollupName): Either[SQLException, Option[String]] =
       using(conn.prepareStatement("select column_name from citus_rollup_distribution_map where resource_name=? and rollup_name=?")) { stmt =>
         stmt.setString(1, resourceName.name)
         stmt.setString(2, rollupName.underlying)
         extractCitusDistributionColumnName(stmt.executeQuery())
       }
-    }
 
     class CitusPrecondition extends CustomPrecondition {
-      override def check(database: Database): Unit = {
-        if (!isCitus) {
-          throw new CustomPreconditionFailedException("Not a citus node")
-        }
-      }
+      override def check(database: Database): Unit =
+        if (!isCitus) { throw new CustomPreconditionFailedException("Not a citus node") }
     }
   }
 }
