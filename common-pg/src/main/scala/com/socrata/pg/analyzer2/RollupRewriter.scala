cd1658b3d9b856fa81f93a3087a8509178601288
Robert J. Macomber <robert.macomber@socrata.com>
2023-08-24 13:20:51 -0700

    More rollup-rewriting experiments

--- common-pg/src/main/scala/com/socrata/pg/analyzer2/RollupRewriter.scala
+++ common-pg/src/main/scala/com/socrata/pg/analyzer2/RollupRewriter.scala
@@ -1,6 +1,10 @@
 package com.socrata.pg.analyzer2
 
+import scala.collection.{mutable => scm}
+
 import com.socrata.soql.analyzer2._
+import com.socrata.soql.collection.OrderedMap
+import com.socrata.soql.environment.ColumnName
 
 import com.socrata.pg.store.PGSecondaryUniverse
 
@@ -35,4 +39,358 @@ object RollupRewriter {
 
   def fromPostgres[MT <: MetaTypes](pgu: PGSecondaryUniverse[MT#ColumnType, MT#ColumnValue]) =
     new Noop[MT]
+}
+
+trait RRExperiments[MT <: MetaTypes] extends SqlizerUniverse[MT] {
+  protected implicit def dtnOrdering: Ordering[MT#DatabaseColumnNameImpl]
+  protected val labelProvider: LabelProvider
+
+  // see if there's a rollup that can be used to answer _this_ select
+  // (not any sub-parts of the select!).  This needs to produce a
+  // statement with the same output schema (in terms of column labels
+  // and types) as the given select.
+  protected def rollupSelectExact(select: Select): Option[Statement]
+
+  // See if there's a rollup that can be used to answer _this_
+  // combined tables (not any sub-parts of the combined tables!).
+  // This needs to produce a statement with the same output schema (in
+  // terms of column labels and types) as the given select.
+  protected def rollupCombinedExact(combined: CombinedTables): Option[Statement]
+
+  final def rollup(stmt: Statement): Option[Statement] = {
+    rollup(stmt, prefixesAllowed = true)
+  }
+
+  private def rollup(stmt: Statement, prefixesAllowed: Boolean): Option[Statement] = {
+    stmt match {
+      case select: Select =>
+        rollupSelectExact(select).
+          orElse(if(prefixesAllowed) rollupPrefixes(select) else None).
+          orElse(rollupSubqueries(select))
+      case v: Values =>
+        None
+      case combined@CombinedTables(op, left, right) =>
+        rollupCombinedExact(combined).orElse {
+          val newLeft = rollup(left)
+          val newRight = rollup(right)
+          if(newLeft.isEmpty && newRight.isEmpty) {
+            None
+          } else {
+            Some(CombinedTables(op, newLeft.getOrElse(left), newRight.getOrElse(right)))
+          }
+        }
+      case _ : CTE =>
+        None // don't have these yet, just punting on them...
+    }
+  }
+
+  // This is a little awkward because rolling up a join-prefix is the
+  // one case where schemas pre-rewrite and post-rewrite can change.
+  // In particular, say we've got
+  //
+  //  select blah from @a join @b on cond where whatever etc
+  //
+  // and we have a rollup which is defined as "select @a.*, @b.* from @a join @b on cond".
+  // In this case we can rewrite this as
+  //
+  //  select blah from (select the, demanded, columns from that_rollup) as @c where whatever
+  //
+  // BUT: column references in the analysis before would have expected to
+  // access (virtual) tables @a and @b, and now they all need to reference
+  // virtual table @c.  So doing that rewrite is the reason for the columnMap
+  // stuff, which maps the "old" labels to the "new" labels.
+  //
+  // The most annoying part is that I'm not actually sure this is a
+  // useful thing to do :)
+  private def rollupPrefixes(select: Select): Option[Statement] =
+    rollupPrefixes(select.from, select).map { case (newFrom, columnMap) =>
+      fixupColumnReferences(select.copy(from = newFrom), columnMap)
+    }
+
+  private def rollupPrefixes(from: From, container: Select): Option[(From, Map[Col, VirtCol])] =
+    from match {
+      case af: AtomicFrom =>
+        None
+      case join: Join =>
+        rollupPrefix(join, demandedColumns(join, container)).orElse {
+          // One thing I'm unhappy about here is that we're
+          // recomputing the demandedColumns for each prefix, which
+          // involves a walk over the "container" select.  It'd be
+          // nice not to do that.
+          rollupPrefixes(join.left, container).map { case (newLeft, columnMap) =>
+            val newRight = rollupAtomicFrom(join.right).getOrElse(join.right)
+            (join.copy(left = newLeft, right = newRight), columnMap)
+          }
+        }
+    }
+
+  final def rollupSubqueries(select: Select): Option[Select] = {
+    val (rewroteSomething, newFrom) = select.from.reduceMap[Boolean,MT](
+      { leftmost =>
+        rollupAtomicFrom(leftmost) match {
+          case None => (false, leftmost)
+          case Some(rewritten) => (true, rewritten)
+        }
+      },
+      { (rewroteSomething, joinType, lateral, left, right, on) =>
+        rollupAtomicFrom(right) match {
+          case None => (rewroteSomething, Join(joinType, lateral, left, right, on))
+          case Some(rewritten) => (true, Join(joinType, lateral, left, rewritten, on))
+        }
+      }
+    )
+
+    if(rewroteSomething) Some(select.copy(from = newFrom))
+    else None
+  }
+
+  private def rollupAtomicFrom(from: AtomicFrom): Option[AtomicFrom] = {
+    from match {
+      case FromStatement(stmt, label, resourceName, alias) =>
+        rollup(stmt).map(FromStatement(_, label, resourceName, alias))
+      case other =>
+        None
+    }
+  }
+
+  // see if there's a rollup that matches `select ${all the columns} from $from`...
+  private def rollupPrefix(from: Join, demandedColumns: Map[Col, CT]): Option[(FromStatement, Map[Col, VirtCol])] = {
+    val ord1 = Ordering[(Int, Int)]
+    val ord2 = Ordering[(Int, MT#DatabaseColumnNameImpl)]
+    val orderedDemandedColumns: Seq[(Col, CT)] = demandedColumns.toSeq.
+      sortWith { case ((col1, _ct1), (col2, _ct2)) =>
+        (col1, col2) match {
+          case (VirtCol(AutoTableLabel(atl1), AutoColumnLabel(acl1)),
+                VirtCol(AutoTableLabel(atl2), AutoColumnLabel(acl2))) =>
+            ord1.compare((atl1, acl1), (atl2, acl2)) < 0
+          case (PhysCol(AutoTableLabel(atl1), _, DatabaseColumnName(dtn1)),
+                PhysCol(AutoTableLabel(atl2), _, DatabaseColumnName(dtn2))) =>
+            ord2.compare((atl1, dtn1), (atl2, dtn2)) < 0
+          case (_ : VirtCol, _ : PhysCol) => true
+          case (_ : PhysCol, _ : VirtCol) => false
+        }
+      }
+
+    val newTable = labelProvider.tableLabel()
+    val columnMap: Map[Col, VirtCol] = orderedDemandedColumns.iterator.map { case (col, _typ) =>
+      col -> VirtCol(newTable, labelProvider.columnLabel())
+    }.toMap
+
+    val computedSelectList = OrderedMap() ++ orderedDemandedColumns.iterator.zipWithIndex.map { case ((col, typ), idx) =>
+      columnMap(col).column -> NamedExpr(col.at(typ, AtomicPositionInfo.None), ColumnName(s"column_$idx"), isSynthetic = true)
+    }
+
+    val target =
+      Select[MT](
+        Distinctiveness.Indistinct(),
+        computedSelectList,
+        from,
+        where = None,
+        groupBy = Nil,
+        having = None,
+        orderBy = Nil,
+        limit = None,
+        offset = None,
+        search = None,
+        hint = Set.empty
+      )
+
+    rollup(target, prefixesAllowed = false).map { rewritten =>
+      (FromStatement(rewritten, newTable, None, None), columnMap)
+    }
+  }
+
+  private sealed abstract class Col {
+    def at(typ: CT, pos: AtomicPositionInfo): Column
+  }
+  private case class PhysCol(table: AutoTableLabel, canonicalName: CanonicalName, column: DatabaseColumnName) extends Col {
+    def at(typ: CT, pos: AtomicPositionInfo) = PhysicalColumn(table, canonicalName, column, typ)(pos)
+  }
+  private case class VirtCol(table: AutoTableLabel, column: AutoColumnLabel) extends Col {
+    def at(typ: CT, pos: AtomicPositionInfo) = VirtualColumn(table, column, typ)(pos)
+  }
+
+  private def demandedColumns(prefix: Join, sel: Select): Map[Col, CT] = {
+    class ColumnsFromFrom(from: Set[AutoTableLabel]) {
+      val acc = new scm.HashMap[Col, CT]
+
+      def go(stmt: Statement): Unit = {
+        stmt match {
+          case CombinedTables(_op, left, right) =>
+            go(left)
+            go(right)
+          case CTE(_defLbl, _defAlias, defQ, _matHint, useQ) =>
+            go(defQ)
+            go(useQ)
+          case Values(_labels, values) =>
+            for {
+              row <- values
+              expr <- row
+            } {
+              go(expr)
+            }
+          case Select(distinctiveness, selectList, from, where, groupBy, having, orderBy, _limit, _offset, _search, _hint) =>
+            go(distinctiveness)
+            for(namedExpr <- selectList.values) go(namedExpr)
+            go(from) // sadly, because of lateral joins, we need to recurse here
+            for(w <- where) go(w)
+            for(gb <- groupBy) go(gb)
+            for(h <- having) go(h)
+            for(ob <- orderBy) go(ob)
+        }
+      }
+
+      def go(expr: Expr): Unit =
+        expr match {
+          case VirtualColumn(table, col, typ) if from(table) =>
+            acc += VirtCol(table, col) -> typ
+          case PhysicalColumn(table, canonName, col, typ) if from(table) =>
+            acc += PhysCol(table, canonName, col) -> typ
+          case FunctionCall(_func, args) =>
+            for(arg <- args) go(arg)
+          case AggregateFunctionCall(_func, args, _distinct, filter) =>
+            for(arg <- args) go(arg)
+            for(expr <- filter) go(expr)
+          case WindowedFunctionCall(_func, args, filter, partitionBy, orderBy, _frame) =>
+            for(arg <- args) go(arg)
+            for(expr <- filter) go(expr)
+            for(expr <- partitionBy) go(expr)
+            for(ob <- orderBy) go(ob)
+          case _ =>
+            ()
+        }
+
+      def go(ob: OrderBy): Unit = go(ob.expr)
+      def go(ne: NamedExpr): Unit = go(ne.expr)
+      def go(distinctiveness: Distinctiveness): Unit =
+        distinctiveness match {
+          case Distinctiveness.Indistinct() | Distinctiveness.FullyDistinct() => ()
+          case Distinctiveness.On(exprs) => for(e <- exprs) go(e)
+        }
+
+      def go(from: From): Unit =
+        from.reduce[Unit](
+          goAtomicFrom(_),
+          { (_, join) =>
+            goAtomicFrom(join.right)
+            go(join.on)
+          }
+        )
+
+      def goAtomicFrom(from: AtomicFrom): Unit =
+        from match {
+          case _ : FromSingleRow => ()
+          case _ : FromTable => ()
+          case fs: FromStatement => go(fs.statement)
+        }
+    }
+
+    def fromTables(from: From): Set[AutoTableLabel] = {
+      from.reduce[Set[AutoTableLabel]](
+        { leftmost => Set(leftmost.label) },
+        { (acc, join) => acc + join.right.label }
+      )
+    }
+
+    val walk = new ColumnsFromFrom(fromTables(prefix))
+    walk.go(sel)
+    val r = walk.acc.toMap
+    println("demanded cols: " + r)
+    r
+  }
+
+  private def fixupColumnReferences(sel: Select, columnMap: Map[Col, VirtCol]): Statement = {
+    def goStmt(stmt: Statement): Statement = {
+      stmt match {
+        case ct@CombinedTables(_op, left, right) =>
+          ct.copy(left = goStmt(left), right = goStmt(right))
+        case cte@CTE(_defLbl, _defAlias, defQ, _matHint, useQ) =>
+          cte.copy(definitionQuery = goStmt(defQ), useQuery = goStmt(useQ))
+        case v@Values(_labels, values) =>
+          v.copy(values = values.map { row => row.map(goExpr(_)) })
+        case Select(distinctiveness, selectList, from, where, groupBy, having, orderBy, limit, offset, search, hint) =>
+          Select(
+            goDistinct(distinctiveness),
+            OrderedMap() ++ selectList.iterator.map { case (label, ne) => label -> goNamedExpr(ne) },
+            goFrom(from),
+            where.map(goExpr(_)),
+            groupBy.map(goExpr(_)),
+            having.map(goExpr(_)),
+            orderBy.map(goOrderBy(_)),
+            limit,
+            offset,
+            search,
+            hint
+          )
+      }
+    }
+
+    def goFrom(from: From): From =
+      from.map[MT](
+        goAtomicFrom(_),
+        (joinType, lateral, left, right, on) => Join(joinType, lateral, left, goAtomicFrom(right), goExpr(on))
+      )
+
+    def goAtomicFrom(af: AtomicFrom): AtomicFrom =
+      af match {
+        case fs: FromStatement => fs.copy(statement = goStmt(fs.statement))
+        case fsr: FromSingleRow => fsr
+        case ft: FromTable => ft
+      }
+
+    def goDistinct(distinct: Distinctiveness): Distinctiveness = {
+      distinct match {
+        case Distinctiveness.On(exprs) =>
+          Distinctiveness.On(exprs.map(goExpr(_)))
+        case other@(Distinctiveness.Indistinct() | Distinctiveness.FullyDistinct()) =>
+          other
+      }
+    }
+
+    def goExpr(expr: Expr): Expr =
+      expr match {
+        case vc@VirtualColumn(table, col, typ) =>
+          columnMap.get(VirtCol(table, col)) match {
+            case Some(VirtCol(newTable, newCol)) =>
+              VirtualColumn(newTable, newCol, typ)(vc.position)
+            case None =>
+              vc
+          }
+        case pc@PhysicalColumn(table, canonName, col, typ) =>
+          columnMap.get(PhysCol(table, canonName, col)) match {
+            case Some(VirtCol(newTable, newCol)) =>
+              VirtualColumn(newTable, newCol, typ)(pc.position)
+            case None =>
+              pc
+          }
+        case fc@FunctionCall(func, args) =>
+          FunctionCall(func, args.map(goExpr(_)))(fc.position)
+        case afc@AggregateFunctionCall(func, args, distinct, filter) =>
+          AggregateFunctionCall(
+            func,
+            args.map(goExpr(_)),
+            distinct,
+            filter.map(goExpr(_))
+          )(afc.position)
+        case wfc@WindowedFunctionCall(func, args, filter, partitionBy, orderBy, frame) =>
+          WindowedFunctionCall(
+            func,
+            args.map(goExpr(_)),
+            filter.map(goExpr(_)),
+            partitionBy.map(goExpr(_)),
+            orderBy.map(goOrderBy(_)),
+            frame
+          )(wfc.position)
+        case other =>
+          other
+      }
+
+    def goOrderBy(ob: OrderBy): OrderBy =
+      ob.copy(expr = goExpr(ob.expr))
+
+    def goNamedExpr(ne: NamedExpr): NamedExpr =
+      ne.copy(expr = goExpr(ne.expr))
+
+    goStmt(sel)
+  }
 }
---
+++ common-pg/src/test/scala/com/socrata/pg/analyzer2/RRExperimentsTest.scala
@@ -0,0 +1,45 @@
+package com.socrata.pg.analyzer2
+
+import org.scalatest.{FunSuite, MustMatchers}
+
+import com.socrata.soql.analyzer2._
+import com.socrata.soql.analyzer2.mocktablefinder._
+import com.socrata.soql.environment.ResourceName
+
+class RRExperimentsTest extends FunSuite with MustMatchers with SqlizerUniverse[SqlizerTest.TestMT] {
+  def tableFinder(items: ((Int, String), Thing[Int, TestType])*) =
+    new MockTableFinder[SqlizerTest.TestMT](items.toMap)
+
+  val analyzer = new SoQLAnalyzer[SqlizerTest.TestMT](TestTypeInfo, TestFunctionInfo)
+
+  test("huh") {
+    val tf = tableFinder(
+      (0, "twocol") -> D("text" -> TestText, "num" -> TestNumber),
+      (0, "english") -> D("num" -> TestNumber, "name" -> TestText),
+      (0, "counts_by_name") -> Q(0, "twocol", "select text, @english.name, count(*) join @english on num = @english.num group by text, @english.name")
+    )
+
+    val Right(foundTables) = tf.findTables(0, ResourceName("counts_by_name"))
+    val analysis = analyzer(foundTables, UserParameters.empty) match {
+      case Right(a) => a
+      case Left(e) => fail(e.toString)
+    }
+
+    val expr = new RRExperiments[SqlizerTest.TestMT] {
+      override val labelProvider = analysis.labelProvider
+      implicit val dtnOrdering = Ordering.String
+
+      override def rollupSelectExact(select: Select): Option[Statement] = {
+        println("rollup select exect: " + select.debugDoc)
+        None
+      }
+
+      override def rollupCombinedExact(combined: CombinedTables): Option[Statement] = {
+        println("rollup combined exact: " + combined.debugDoc)
+        None
+      }
+    }
+
+    val result = expr.rollup(analysis.statement).map(_.debugStr)
+  }
+}
