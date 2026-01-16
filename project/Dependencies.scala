3ea616ce814c0505b9689937b70cf5dc0d2a94a0
Peaches Gall <peaches.gall@tylertech.com>
2026-01-16 15:31:13 -0800

    EN-86364: Provide username and password to ActiveMQ when configured (#767)
    Co-authored-by: Robert J. Macomber <robert.macomber@socrata.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     val soqlStdlib = "4.14.90"
     val simpleCsv = "0.0.2"
     val typesafeConfig = "1.0.0"
-    val dataCoordinator = "4.2.43"
+    val dataCoordinator = "4.2.46"
     val typesafeScalaLogging = "3.9.2"
     val rojomaJson = "3.13.0"
     val metrics = "4.2.30"
